ed3725078c4b109fc449f977030dce04b6c3b2d6
Fahid M <fahidroid@gmail.com>
2017-01-15 23:45:48 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log=(`${now}:== ${req.method}=== ${req.url}`)
+  fs.appendFile("server-log.txt",log+'\n',(err)=>{
+    if(err)
+        console.log('Unable to append to server.log')
+  })
+  next();
+})
+app.use((req,res,next)=>{
+  res.render('maintain.hbs');
+
+})
+app.use(express.static(__dirname+'/public'))
+hbs.registerHelper('getCurrentYear',()=> 2016)
+hbs.registerHelper('screamIt',(text)=> text.toUpperCase())
+// app.get('/',(req,res)=>{
+//   // res.send("<h1>Welcome to my first web app</h1>")
+//   res.send({
+//     name: "Andrew",
+//     class: "XII -F",
+//     age: "67",
+//     profession: "teacher"
+//
+//   })
+// })
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page2',
+    welcomeMessage: "Welcome to my About site"
+
+  })
+})
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    error_code: 404,
+    error_message: "Content Not Found"
+
+  })
+})
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my site"
+
+  })
+})
+app.listen(9000,()=>{
+  console.log("Server is started at 3001")
+})
